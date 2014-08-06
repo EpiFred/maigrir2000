2586f66bda9bd2dbef2c62bb9557cde71477ce08
kevin_nougat <kevin.nougat@epitech.eu>
2014-08-06 16:53:24 +0200

    Remove useless import Regime.java (project clean)

--- Maigrir2000v03/src/com/maigrir2000v03/Regime.java
+++ Maigrir2000v03/src/com/maigrir2000v03/Regime.java
@@ -1,18 +1,13 @@
 package com.maigrir2000v03;
 
-import android.location.GpsStatus.Listener;
 import android.os.Bundle;
-import android.app.Activity;
 import android.app.ListActivity;
 import android.content.Intent;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
-import android.widget.Toast;
 import android.support.v4.app.NavUtils;
 
 public class Regime extends ListActivity {
