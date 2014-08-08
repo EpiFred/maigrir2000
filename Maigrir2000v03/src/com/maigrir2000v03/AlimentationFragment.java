2ab496bdda7c058d9a93c1d4f99cc50be0af9fe2
kevin_nougat <kevin.nougat@epitech.eu>
2014-08-08 19:38:25 +0200

    Listview on nutriment and alimentation + add simple_listview_row + test styles

--- Maigrir2000v03/src/com/maigrir2000v03/AlimentationFragment.java
+++ Maigrir2000v03/src/com/maigrir2000v03/AlimentationFragment.java
@@ -5,8 +5,21 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class AlimentationFragment extends Fragment {
+	
+	private ListView ListAlimentation;
+	private ArrayAdapter<String> adapter;
+	
+	private String[] alimentation = {
+			"Composer son repas", 
+			"Les Additifs Alimentaires", 
+			"Les Produits Allégés", 
+			"Déchiffrer les étiquettes", 
+			"La Malnutrition", 
+			};
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
@@ -15,4 +28,13 @@ public class AlimentationFragment extends Fragment {
 		return rootView;
 	}
 
+	@Override
+	public void onActivityCreated(Bundle savedInstanceState) {
+		super.onActivityCreated(savedInstanceState);
+		
+		adapter = new ArrayAdapter<String>(getActivity(),R.layout.simple_listview_row, alimentation);
+		ListAlimentation = (ListView) getView().findViewById(R.id.ListAlimentation);
+		ListAlimentation.setAdapter(adapter);
+
+	}
 }
--- Maigrir2000v03/src/com/maigrir2000v03/NutrimentFragment.java
+++ Maigrir2000v03/src/com/maigrir2000v03/NutrimentFragment.java
@@ -1,12 +1,29 @@
 package com.maigrir2000v03;
 
 import android.os.Bundle;
+
 import android.support.v4.app.Fragment;
+import android.support.v4.app.ListFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class NutrimentFragment extends Fragment {
+	
+	private ListView ListNutriment;
+	private ArrayAdapter<String> adapter;
+	
+	private String[] nutriments = {
+			"Les Protéines", 
+			"Les Glucides", 
+			"Les Lipides", 
+			"Vitamines - Sels Mineraux", 
+			"Les Fibres", 
+			"L'Eau", 
+			};
+	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
@@ -15,4 +32,13 @@ public class NutrimentFragment extends Fragment {
 		return rootView;
 	}
 
+	@Override
+	public void onActivityCreated(Bundle savedInstanceState) {
+		super.onActivityCreated(savedInstanceState);
+		
+		adapter = new ArrayAdapter<String>(getActivity(), R.layout.simple_listview_row, nutriments);
+		ListNutriment = (ListView) getView().findViewById(R.id.ListNutriment);
+		ListNutriment.setAdapter(adapter);
+
+	}
 }
